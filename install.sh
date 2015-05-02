1408bec2f902a29dcee0a6ab58af79635443bd93
gisphm <phmfk@hotmail.com>
2015-05-03 01:48:55 +0800

    remove neobundleinstall
    Signed-off-by: gisphm <phmfk@hotmail.com>

--- install.sh
+++ install.sh
@@ -5,4 +5,3 @@ mkdir -p ~/.vim/tmp/swap
 mkdir -p ~/.vim/tmp/undo
 mkdir -p ~/.vim/tmp/view
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-vim +NeoBundleInstall +qall
