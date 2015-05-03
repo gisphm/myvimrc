0679bef8faee8159f40f4eff7eb11468dc33a49f
gisphm <phmfk@hotmail.com>
2015-05-03 19:36:58 +0800

    install neobundle if not existed
    Signed-off-by: gisphm <phmfk@hotmail.com>

--- install.sh
+++ install.sh
@@ -21,4 +21,4 @@ mkdir -p ~/.vim/tmp/backup
 mkdir -p ~/.vim/tmp/swap
 mkdir -p ~/.vim/tmp/undo
 mkdir -p ~/.vim/tmp/view
-git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+[[ -d ~/.vim/bundle/neobundle.vim/ ]] || git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
